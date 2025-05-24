14483e8da1bc5fbe7700c4fad531f6af5e31a46a
Iradukunda Belyse <birad2020@gmail.com>
2025-05-24 05:07:01 +0200

    update 0-javascript_is_amazing

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -2,10 +2,5 @@
 const myVar = "JavaScript is amazing";
 
 console.log(myVar);
-//Output: JavaScript is amazing
-git add 0-javascript_is_amazing.js
-git commit -m "Initial commit"
-git push origin main
-//Output: [main 1234567] Initial commit
-// 1 file changed, 1 insertion(+)
+
 
